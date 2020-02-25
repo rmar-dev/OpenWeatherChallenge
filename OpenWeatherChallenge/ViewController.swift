672ffa186757ee2eae38f02ff01080c70fdabca8
Ricardo Rabeto <ricardomarabeto@gmail.com>
2020-02-25 18:09:18 +0000

    Initial Commit

---
+++ OpenWeatherChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenWeatherChallenge
+//
+//  Created by Ricardo Rabeto on 25/02/2020.
+//  Copyright © 2020 Ricardo Rabeto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
